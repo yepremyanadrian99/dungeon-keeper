1a08b14c36e6e213cb4f57b083d280ae5be885b6
Yepremyan <adrian.yepremyan@picsart.com>
2022-05-21 17:59:41 +0400

    small spring context change.

--- src/main/java/am/adrian/dungeonkeeper/Application.java
+++ src/main/java/am/adrian/dungeonkeeper/Application.java
@@ -1,6 +1,5 @@
 package am.adrian.dungeonkeeper;
 
-import am.adrian.dungeonkeeper.config.GameConfig;
 import am.adrian.dungeonkeeper.config.GeneralConfig;
 import am.adrian.dungeonkeeper.game.ConsoleGame;
 import org.springframework.context.ApplicationContext;
@@ -9,10 +8,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Application {
 
     public static void main(String[] args) {
-        final ApplicationContext context = new AnnotationConfigApplicationContext(
-                GeneralConfig.class,
-                GameConfig.class
-        );
+        final ApplicationContext context = new AnnotationConfigApplicationContext(GeneralConfig.class);
         final ConsoleGame game = context.getBean(ConsoleGame.class);
         game.start();
     }
