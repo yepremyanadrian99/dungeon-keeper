6affe0d6f59a240d9570f5287ce4832a1e33d2cc
Yepremyan <adrian.yepremyan@picsart.com>
2022-05-21 18:25:14 +0400

    added log4j2 with file and console appenders.

--- src/main/java/am/adrian/dungeonkeeper/game/ConsoleGame.java
+++ src/main/java/am/adrian/dungeonkeeper/game/ConsoleGame.java
@@ -3,15 +3,19 @@ package am.adrian.dungeonkeeper.game;
 import am.adrian.dungeonkeeper.constant.GameState;
 import am.adrian.dungeonkeeper.renderer.GameMapRenderer;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @RequiredArgsConstructor
 public class ConsoleGame implements Runnable {
+
+    private static final Logger logger = LogManager.getLogger(ConsoleGame.class);
 
     private final GameMapRenderer renderer;
     private final GameStateService stateService;
 
     public void start() {
-        System.out.println("Game started");
+        logger.info("Game started");
         stateService.setState(GameState.STARTED);
         run();
     }
